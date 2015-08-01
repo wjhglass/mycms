4adf882b187cc24eb607d149b3b0e81ec66732a0
wjhglass <wjhglass@gmail.com>
2015-08-01 17:48:00 +0800

    static page ok

--- templates_c/1d7c7a527b6335cc7a623305ca940e1findex.tpl.php
+++ templates_c/1d7c7a527b6335cc7a623305ca940e1findex.tpl.php
@@ -154,23 +154,177 @@
 	<!--rec end-->
 	
 	<!--sidebar-right start-->
-	<div class="sidebar-right">sidebar-right</div>
+	<div class="sidebar-right">
+		<div class="adver">
+			<img width="270" height="200" alt="广告" src="images/adver2.png" />
+		</div>
+		<div class="hot">
+			<h2>本月热点</h2>
+			<ul>
+				<li><em>06-20</em><a href="javascript:;">银监会否认首套房贷首付将提至...</a></li>
+				<li><em>04-02</em><a href="javascript:;">发改委曝房价违规开发商名单央...</a></li>
+				<li><em>02-13</em><a href="javascript:;">社科院预测更严厉楼市政策年内...</a></li>
+				<li><em>05-05</em><a href="javascript:;">比亚迪拟“缩水”回归A股 以缓解...</a></li>
+				<li><em>07-11</em><a href="javascript:;">第一线：北京限制高价盘预售证...</a></li>
+				<li><em>03-18</em><a href="javascript:;">电网主辅分离或年内完成 葛洲坝...</a></li>
+				<li><em>05-02</em><a href="javascript:;">京沪高铁将于6月9日起试运行10天...</a></li>
+			</ul>
+		</div>
+		<div class="comm">
+			<h2>本月评论</h2>
+			<ul>
+				<li><em>06-20</em><a href="javascript:;">银监会否认首套房贷首付将提至...</a></li>
+				<li><em>04-02</em><a href="javascript:;">发改委曝房价违规开发商名单央...</a></li>
+				<li><em>02-13</em><a href="javascript:;">社科院预测更严厉楼市政策年内...</a></li>
+				<li><em>05-05</em><a href="javascript:;">比亚迪拟“缩水”回归A股 以缓解...</a></li>
+				<li><em>07-11</em><a href="javascript:;">第一线：北京限制高价盘预售证...</a></li>
+				<li><em>03-18</em><a href="javascript:;">电网主辅分离或年内完成 葛洲坝...</a></li>
+				<li><em>05-02</em><a href="javascript:;">京沪高铁将于6月9日起试运行10天...</a></li>
+			</ul>
+		</div>
+		<div class="vote">
+			<h2>调查投票</h2>
+			<h3>请问您是怎么知道本站的：</h3>
+			<form>
+				<label><input type="radio" name="vote" checked="checked" /> 门户网站的搜索引擎</label>
+				<label><input type="radio" name="vote" /> Google或百度搜索</label>
+				<label><input type="radio" name="vote" /> 别的网站上的链接</label>
+				<label><input type="radio" name="vote" /> 朋友介绍或者电视广告</label>
+				<p><input type="submit" value="投票" name="send" /> <input type="button" value="查看" /></p>
+			</form>
+		</div>
+	</div>
 	<!--sidebar-right end-->
 	
 	<!--picnews start-->
-	<div class="picnews">picnews</div>
+	<div class="picnews">
+		<h2>图文资讯</h2>
+		<dl>
+			<dt><a href="javascript:;"><img width="150" height="110" src="images/pic1.png" alt="标题" /></a></dt>
+			<dd><a href="javascript:;">以色列总理出访法国 士兵在迎接仪式上晕倒</a></dd>
+		</dl>
+		<dl>
+			<dt><a href="javascript:;"><img width="150" height="110" src="images/pic2.png" alt="标题" /></a></dt>
+			<dd><a href="javascript:;">江西数百学生操场上给母亲洗脚</a></dd>
+		</dl>
+		<dl>
+			<dt><a href="javascript:;"><img width="150" height="110" src="images/pic3.png" alt="标题" /></a></dt>
+			<dd><a href="javascript:;">歼20照片再现 地勤人员钻入起落架舱</a></dd>
+		</dl>
+		<dl>
+			<dt><a href="javascript:;"><img width="150" height="110" src="images/pic4.png" alt="标题" /></a></dt>
+			<dd><a href="javascript:;">摄影师拍“水下工程” 波浪如蘑菇云</a></dd>
+		</dl>	
+	</div>
 	<!--picnews end-->
 	
 	<!--newslist start-->
-	<div class="newslist">newslist</div>
+	<div class="newslist">
+		<div class="list bottom">
+			<h2><a href="javascript:;">更多</a>军事动态</h2>
+			<ul>
+				<li><em>06-04</em><a href="javascript:;">报告预计2011年全球3D电视出货量同比增500%...</a></li>
+				<li><em>05-03</em><a href="javascript:;">58同城获日本分类信息公司Recruit战略...</a></li>
+				<li><em>06-04</em><a href="javascript:;">DisplaySearch：全球3D电视销量将达22OO万台..</a></li>
+				<li><em>04-08</em><a href="javascript:;">前程无忧一季度净利1400万美元 同比增81.6%...</a></li>
+				<li><em>08-04</em><a href="javascript:;">人人上市次日早盘大跌9%报16.39美元...</a></li>
+				<li><em>11-18</em><a href="javascript:;">优酷来自手机及平板电脑播放量已占整站10%...</a></li>
+				<li><em>06-12</em><a href="javascript:;">报告称Android一季度在美市场份额扩至34.7%...</a></li>
+				<li><em>12-11</em><a href="javascript:;">阿尔卡特朗讯2011第一财季营收同比增15%...</a></li>
+				<li><em>01-09</em><a href="javascript:;">HTC第一季度智能手机销量970万 年成长192% ...</a></li>
+				<li><em>04-07</em><a href="javascript:;">一季度苹果占全球智能机份额升至18.7%席会...</a></li>
+				<li><em>08-04</em><a href="javascript:;">人人上市次日早盘大跌9%报16.39美元...</a></li>
+			</ul>
+		</div>
+		<div class="list right bottom">
+			<h2><a href="javascript:;">更多</a>八卦娱乐</h2>
+			<ul>
+				<li><em>06-04</em><a href="javascript:;">报告预计2011年全球3D电视出货量同比增500%...</a></li>
+				<li><em>05-03</em><a href="javascript:;">58同城获日本分类信息公司Recruit战略...</a></li>
+				<li><em>06-04</em><a href="javascript:;">DisplaySearch：全球3D电视销量将达22OO万台..</a></li>
+				<li><em>04-08</em><a href="javascript:;">前程无忧一季度净利1400万美元 同比增81.6%...</a></li>
+				<li><em>08-04</em><a href="javascript:;">人人上市次日早盘大跌9%报16.39美元...</a></li>
+				<li><em>11-18</em><a href="javascript:;">优酷来自手机及平板电脑播放量已占整站10%...</a></li>
+				<li><em>06-12</em><a href="javascript:;">报告称Android一季度在美市场份额扩至34.7%...</a></li>
+				<li><em>12-11</em><a href="javascript:;">阿尔卡特朗讯2011第一财季营收同比增15%...</a></li>
+				<li><em>01-09</em><a href="javascript:;">HTC第一季度智能手机销量970万 年成长192% ...</a></li>
+				<li><em>04-07</em><a href="javascript:;">一季度苹果占全球智能机份额升至18.7%席会...</a></li>
+				<li><em>08-04</em><a href="javascript:;">人人上市次日早盘大跌9%报16.39美元...</a></li>
+			</ul>
+		</div>
+		<div class="list">
+			<h2><a href="javascript:;">更多</a>时尚女人</h2>
+			<ul>
+				<li><em>06-04</em><a href="javascript:;">报告预计2011年全球3D电视出货量同比增500%...</a></li>
+				<li><em>05-03</em><a href="javascript:;">58同城获日本分类信息公司Recruit战略...</a></li>
+				<li><em>06-04</em><a href="javascript:;">DisplaySearch：全球3D电视销量将达22OO万台..</a></li>
+				<li><em>04-08</em><a href="javascript:;">前程无忧一季度净利1400万美元 同比增81.6%...</a></li>
+				<li><em>08-04</em><a href="javascript:;">人人上市次日早盘大跌9%报16.39美元...</a></li>
+				<li><em>11-18</em><a href="javascript:;">优酷来自手机及平板电脑播放量已占整站10%...</a></li>
+				<li><em>06-12</em><a href="javascript:;">报告称Android一季度在美市场份额扩至34.7%...</a></li>
+				<li><em>12-11</em><a href="javascript:;">阿尔卡特朗讯2011第一财季营收同比增15%...</a></li>
+				<li><em>01-09</em><a href="javascript:;">HTC第一季度智能手机销量970万 年成长192% ...</a></li>
+				<li><em>04-07</em><a href="javascript:;">一季度苹果占全球智能机份额升至18.7%席会...</a></li>
+				<li><em>08-04</em><a href="javascript:;">人人上市次日早盘大跌9%报16.39美元...</a></li>
+			</ul>
+		</div>
+		<div class="list right">
+			<h2><a href="javascript:;">更多</a>科技频道</h2>
+			<ul>
+				<li><em>06-04</em><a href="javascript:;">报告预计2011年全球3D电视出货量同比增500%...</a></li>
+				<li><em>05-03</em><a href="javascript:;">58同城获日本分类信息公司Recruit战略...</a></li>
+				<li><em>06-04</em><a href="javascript:;">DisplaySearch：全球3D电视销量将达22OO万台..</a></li>
+				<li><em>04-08</em><a href="javascript:;">前程无忧一季度净利1400万美元 同比增81.6%...</a></li>
+				<li><em>08-04</em><a href="javascript:;">人人上市次日早盘大跌9%报16.39美元...</a></li>
+				<li><em>11-18</em><a href="javascript:;">优酷来自手机及平板电脑播放量已占整站10%...</a></li>
+				<li><em>06-12</em><a href="javascript:;">报告称Android一季度在美市场份额扩至34.7%...</a></li>
+				<li><em>12-11</em><a href="javascript:;">阿尔卡特朗讯2011第一财季营收同比增15%...</a></li>
+				<li><em>01-09</em><a href="javascript:;">HTC第一季度智能手机销量970万 年成长192% ...</a></li>
+				<li><em>04-07</em><a href="javascript:;">一季度苹果占全球智能机份额升至18.7%席会...</a></li>
+				<li><em>08-04</em><a href="javascript:;">人人上市次日早盘大跌9%报16.39美元...</a></li>
+			</ul>
+		</div>
+	</div>
 	<!--newslist end-->
 	
 	<!--link start-->
-	<div class="links">links</div>
+	<div class="links">
+		<h2><span><a href="javascript:;">所有链接</a> | <a href="javascript:;">申请加入</a></span>友情链接</h2>
+		<ul>
+			<li><a href="javascript:;">网易新闻</a></li>
+			<li><a href="javascript:;">百度搜索</a></li>
+			<li><a href="javascript:;">优酷视频</a></li>
+			<li><a href="javascript:;">搜狐</a></li>
+			<li><a href="javascript:;">中国站长站</a></li>
+			<li><a href="javascript:;">中国中央电视台</a></li>
+			<li><a href="javascript:;">腾讯网</a></li>
+			<li><a href="javascript:;">阿里巴巴</a></li>
+			<li><a href="javascript:;">淘宝网</a></li>
+			<li><a href="javascript:;">分享互联网</a></li>
+			<li><a href="javascript:;">盛大游戏在线</a></li>
+			<li><a href="javascript:;">凤凰网</a></li>
+			<li><a href="javascript:;">太平洋电脑网</a></li>
+			<li><a href="javascript:;">当当网</a></li>
+		</ul>
+		<dl>
+			<dd><a href="javascript:;" target="_blank"><img src="images/youku.png" alt="网站名" /></a></dd>
+			<dd><a href="javascript:;" target="_blank"><img src="images/163.png" alt="网站名" /></a></dd>
+			<dd><a href="javascript:;" target="_blank"><img src="images/baidu.png" alt="网站名" /></a></dd>
+			<dd><a href="javascript:;" target="_blank"><img src="images/damai.png" alt="网站名" /></a></dd>
+			<dd><a href="javascript:;" target="_blank"><img src="images/kugou.png" alt="网站名" /></a></dd>
+			<dd><a href="javascript:;" target="_blank"><img src="images/qqcar.png" alt="网站名" /></a></dd>
+			<dd><a href="javascript:;" target="_blank"><img src="images/yiche.png" alt="网站名" /></a></dd>
+			<dd><a href="javascript:;" target="_blank"><img src="images/alibaba.png" alt="网站名" /></a></dd>
+			<dd><a href="javascript:;" target="_blank"><img src="images/sina.png" alt="网站名" /></a></dd>	
+		</dl>
+	</div>
 	<!--link end-->
 	
 	<!--footer start-->
-	<div class="footer">footer</div>
+	<div class="footer">
+		<p>Powered by <span>diaosi.com</span> (C) 2004-2011 DesDev Inc.</p>
+		<p>Copyright (C) 2004-2011 diaosiCMS. <span>屌丝俱乐部</span> 版权所有</p>
+	</div>
 	<!--footer end-->
 </body>
 </html>

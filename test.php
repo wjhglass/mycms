6611ecd95cbaeb7805d995b82a55407095d64961
wjhglass <wjhglass@gmail.com>
2015-08-16 16:46:48 +0800

    dddd

--- test.php
+++ test.php
@@ -1,3 +1,17 @@
+<script type="text/javascript" src="ckeditor/ckeditor.js"></script>
+<script type="text/javascript" src="ckeditor/config.js"></script>
+<script type="text/javascript">
+<!--
+CKEDITOR.config.skin='kama'
+//-->
+</script>
+<form method="post">
+	<textarea id="ta1" class="ckeditor" name="content">
+		
+	</textarea>
+	<input type="submit" value="提交" />
+</form>
 <?php
 header ( "Content-type: text/html; charset=utf-8" );
-echo md5('111111');
+echo md5('111111').'<br />';
+echo $_POST['content'].'<br />';
